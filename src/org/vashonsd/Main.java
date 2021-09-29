7981895d77fa5032d541c0d6d6ef6aa009acc7c4
ajames <ajames@vashonsd.org>
2021-09-29 10:45:53 -0700

    Added a comment.

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -2,6 +2,7 @@ package org.vashonsd;
 
 public class Main {
 
+    //Now there is a comment above the main method.
     public static void main(String[] args) {
 	    System.out.println("Hello World");
     }
